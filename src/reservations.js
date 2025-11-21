fa2538ef8ec2b979523715a2f5ee647463dc3e59
novusagent-tpl <novusagent.tpl@gmail.com>
2025-11-21 18:40:21 +0100

    Fix Google credentials for Render deploy

--- src/reservations.js
+++ src/reservations.js
@@ -12,7 +12,10 @@ require('dotenv').config();
 // -----------------------------
 function getSheetsClient() {
   const auth = new google.auth.GoogleAuth({
-    keyFile: path.join(__dirname, '..', 'credentials', 'google-service-account.json'),
+    credentials: {
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n')
+  },
     scopes: ['https://www.googleapis.com/auth/spreadsheets']
   });
 
