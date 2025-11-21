bf30cef151d2ecddf0705cb163d04392f27dcb57
novusagent-tpl <novusagent.tpl@gmail.com>
2025-11-21 18:54:18 +0100

    Fix GOOGLE_PRIVATE_KEY format for Render

--- src/calendar.js
+++ src/calendar.js
@@ -9,12 +9,12 @@ require('dotenv').config();
 // -----------------------------
 function getCalendarClient() {
   const auth = new google.auth.GoogleAuth({
-   credentials: {
-   client_email: process.env.GOOGLE_CLIENT_EMAIL,
-   private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n')
-   },
-    scopes: ['https://www.googleapis.com/auth/calendar']
-  });
+  credentials: {
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY
+  },
+  scopes: ['https://www.googleapis.com/auth/calendar']
+});
 
   return google.calendar({ version: 'v3', auth });
 }
--- src/reservations.js
+++ src/reservations.js
@@ -12,12 +12,12 @@ require('dotenv').config();
 // -----------------------------
 function getSheetsClient() {
   const auth = new google.auth.GoogleAuth({
-    credentials: {
-  client_email: process.env.GOOGLE_CLIENT_EMAIL,
-  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n')
+  credentials: {
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY
   },
-    scopes: ['https://www.googleapis.com/auth/spreadsheets']
-  });
+  scopes: ['https://www.googleapis.com/auth/spreadsheets']
+});
 
   return google.sheets({ version: 'v4', auth });
 }
